90f8936b75121bcd23d33f6d98c74ba445ef99c7
dyw7179 <66932077+dyw7179@users.noreply.github.com>
2025-04-17 16:49:04 +0900

    chore: 의존성 수정. npy생성가능

--- demo.py
+++ demo.py
@@ -1,29 +1,3 @@
-"""
-Demo code
-
-To run our method, you need a bounding box around the person. The person needs to be centered inside the bounding box and the bounding box should be relatively tight. You can either supply the bounding box directly or provide an [OpenPose](https://github.com/CMU-Perceptual-Computing-Lab/openpose) detection file. In the latter case we infer the bounding box from the detections.
-
-In summary, we provide 3 different ways to use our demo code and models:
-1. Provide only an input image (using ```--img```), in which case it is assumed that it is already cropped with the person centered in the image.
-2. Provide an input image as before, together with the OpenPose detection .json (using ```--openpose```). Our code will use the detections to compute the bounding box and crop the image.
-3. Provide an image and a bounding box (using ```--bbox```). The expected format for the json file can be seen in ```examples/im1010_bbox.json```.
-
-Example with OpenPose detection .json
-```
-python3 demo.py --checkpoint=data/model_checkpoint.pt --img=examples/im1010.png --openpose=examples/im1010_openpose.json
-```
-Example with predefined Bounding Box
-```
-python3 demo.py --checkpoint=data/model_checkpoint.pt --img=examples/im1010.png --bbox=examples/im1010_bbox.json
-```
-Example with cropped and centered image
-```
-python3 demo.py --checkpoint=data/model_checkpoint.pt --img=examples/im1010.png
-```
-
-Running the previous command will save the results in ```examples/im1010_{shape,shape_side}.png```. The file ```im1010_shape.png``` shows the overlayed reconstruction of human shape. We also render a side view, saved in ```im1010_shape_side.png```.
-"""
-
 import torch
 from torchvision.transforms import Normalize
 import numpy as np
@@ -33,7 +7,6 @@ import json
 
 from models import hmr, SMPL
 from utils.imutils import crop
-from utils.renderer import Renderer
 import config
 import constants
 
@@ -45,7 +18,6 @@ parser.add_argument('--openpose', type=str, default=None, help='Path to .json co
 parser.add_argument('--outfile', type=str, default=None, help='Filename of output images. If not set use input filename.')
 
 def bbox_from_openpose(openpose_file, rescale=1.2, detection_thresh=0.2):
-    """Get center and scale for bounding box from openpose detections."""
     with open(openpose_file, 'r') as f:
         keypoints = json.load(f)['people'][0]['pose_keypoints_2d']
     keypoints = np.reshape(np.array(keypoints), (-1,3))
@@ -53,35 +25,24 @@ def bbox_from_openpose(openpose_file, rescale=1.2, detection_thresh=0.2):
     valid_keypoints = keypoints[valid][:,:-1]
     center = valid_keypoints.mean(axis=0)
     bbox_size = (valid_keypoints.max(axis=0) - valid_keypoints.min(axis=0)).max()
-    # adjust bounding box tightness
     scale = bbox_size / 200.0
     scale *= rescale
     return center, scale
 
 def bbox_from_json(bbox_file):
-    """Get center and scale of bounding box from bounding box annotations.
-    The expected format is [top_left(x), top_left(y), width, height].
-    """
     with open(bbox_file, 'r') as f:
         bbox = np.array(json.load(f)['bbox']).astype(np.float32)
     ul_corner = bbox[:2]
     center = ul_corner + 0.5 * bbox[2:]
     width = max(bbox[2], bbox[3])
     scale = width / 200.0
-    # make sure the bounding box is rectangular
     return center, scale
 
 def process_image(img_file, bbox_file, openpose_file, input_res=224):
-    """Read image, do preprocessing and possibly crop it according to the bounding box.
-    If there are bounding box annotations, use them to crop the image.
-    If no bounding box is specified but openpose detections are available, use them to get the bounding box.
-    """
     normalize_img = Normalize(mean=constants.IMG_NORM_MEAN, std=constants.IMG_NORM_STD)
-    img = cv2.imread(img_file)[:,:,::-1].copy() # PyTorch does not support negative stride at the moment
+    img = cv2.imread(img_file)[:,:,::-1].copy()
     if bbox_file is None and openpose_file is None:
-        # Assume that the person is centerered in the image
-        height = img.shape[0]
-        width = img.shape[1]
+        height, width = img.shape[:2]
         center = np.array([width // 2, height // 2])
         scale = max(height, width) / 200
     else:
@@ -111,37 +72,29 @@ if __name__ == '__main__':
                 create_transl=False).to(device)
     model.eval()
 
-    # Setup renderer for visualization
-    renderer = Renderer(focal_length=constants.FOCAL_LENGTH, img_res=constants.IMG_RES, faces=smpl.faces)
-
-
     # Preprocess input image and generate predictions
     img, norm_img = process_image(args.img, args.bbox, args.openpose, input_res=constants.IMG_RES)
     with torch.no_grad():
         pred_rotmat, pred_betas, pred_camera = model(norm_img.to(device))
-        pred_output = smpl(betas=pred_betas, body_pose=pred_rotmat[:,1:], global_orient=pred_rotmat[:,0].unsqueeze(1), pose2rot=False)
-        pred_vertices = pred_output.vertices
-        
-    # Calculate camera parameters for rendering
-    camera_translation = torch.stack([pred_camera[:,1], pred_camera[:,2], 2*constants.FOCAL_LENGTH/(constants.IMG_RES * pred_camera[:,0] +1e-9)],dim=-1)
-    camera_translation = camera_translation[0].cpu().numpy()
+        pred_output = smpl(betas=pred_betas,
+                           body_pose=pred_rotmat[:,1:],
+                           global_orient=pred_rotmat[:,0].unsqueeze(1),
+                           pose2rot=False)
+        pred_vertices = pred_output["vertices"]
+
+    # Convert outputs
+    camera_translation = torch.stack([
+        pred_camera[:,1], pred_camera[:,2],
+        2*constants.FOCAL_LENGTH/(constants.IMG_RES * pred_camera[:,0] +1e-9)
+    ], dim=-1)[0].cpu().numpy()
+    
     pred_vertices = pred_vertices[0].cpu().numpy()
     img = img.permute(1,2,0).cpu().numpy()
 
-    
-    # Render parametric shape
-    img_shape = renderer(pred_vertices, camera_translation, img)
-    
-    # Render side views
-    aroundy = cv2.Rodrigues(np.array([0, np.radians(90.), 0]))[0]
-    center = pred_vertices.mean(axis=0)
-    rot_vertices = np.dot((pred_vertices - center), aroundy) + center
-    
-    # Render non-parametric shape
-    img_shape_side = renderer(rot_vertices, camera_translation, np.ones_like(img))
-
+    # Save .npy file
     outfile = args.img.split('.')[0] if args.outfile is None else args.outfile
+    np.save(outfile + '_vertices.npy', pred_vertices)
 
-    # Save reconstructions
-    cv2.imwrite(outfile + '_shape.png', 255 * img_shape[:,:,::-1])
-    cv2.imwrite(outfile + '_shape_side.png', 255 * img_shape_side[:,:,::-1])
+    # (Optional) Save rendered images — 현재 주석 처리
+    # cv2.imwrite(outfile + '_shape.png', 255 * img_shape[:,:,::-1])
+    # cv2.imwrite(outfile + '_shape_side.png', 255 * img_shape_side[:,:,::-1])
